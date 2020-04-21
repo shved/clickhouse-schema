1902726bb8abf808d8294c839533968d9c83e3ee
Vitaly Shvedchenko <vitaly.shvedchenko@gmail.com>
2020-04-21 12:25:05 +0300

    getting tables

--- main.go
+++ main.go
@@ -32,43 +32,71 @@ func writeSchema(url *string, path *string) error {
 	db := connectToClickHouse(url)
 	defer db.Close()
 
-	var databases []string
-	rows, err := db.Query("SHOW DATABASES FORMAT TabSeparated;")
+	databases := getDatabases(db)
+
+	for _, dbName := range databases {
+		fmt.Println("DB:", dbName)
+		tables := getTables(db, dbName)
+		fmt.Println(tables)
+	}
+
+	return nil
+}
+
+func getTables(db *sql.DB, dbName string) []string {
+	var tables []string
+	rows, err := db.Query("SELECT name FROM system.tables WHERE database = ?;", dbName)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("getting tables for %s: %v", dbName, err)
 	}
 	defer rows.Close()
 
 	for rows.Next() {
 		var name string
 		if err := rows.Scan(&name); err != nil {
-			log.Fatal(err)
+			log.Fatalf("getting tables for %s: %v", dbName, err)
+		}
+		tables = append(tables, name)
+	}
+
+	if rows.Err(); err != nil {
+		log.Fatalf("getting tables for %s: %v", dbName, err)
+	}
+
+	return tables
+}
+
+func getDatabases(db *sql.DB) []string {
+	var databases []string
+	rows, err := db.Query("SHOW DATABASES FORMAT TabSeparated;")
+	if err != nil {
+		log.Fatalf("getting databases: %v", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var name string
+		if err := rows.Scan(&name); err != nil {
+			log.Fatalf("getting databases: %v", err)
 		}
 		databases = append(databases, name)
 	}
 
-	if !rows.NextResultSet() {
-		fmt.Println("No databases in given cluster")
-		os.Exit(0)
-	}
-
 	if rows.Err(); err != nil {
-		log.Fatal(err)
+		log.Fatalf("getting databases: %v", err)
 	}
 
-	fmt.Println(databases)
-
-	return nil
+	return databases
 }
 
 func connectToClickHouse(url *string) *sql.DB {
 	db, err := sql.Open("clickhouse", *url)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("getting clickhouse connection: %v", err)
 	}
 
 	if _, err := db.Exec("SELECT 1"); err != nil {
-		log.Fatal("Fail to ping clickhouse. ", err)
+		log.Fatalf("trying to ping clickhouse: %v", err)
 	}
 
 	return db
